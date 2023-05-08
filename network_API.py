c66b900dd10074194ac09851a99e6efc307ac7ae
RfailesDev <86327915+RfailesDev@users.noreply.github.com>
2023-05-08 19:36:17 +0400

    Add files via upload

--- network_API.py
+++ network_API.py
@@ -1,13 +1,16 @@
-from flask import Flask, request
+import socket
 
-import API_system
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock.bind(('rfailesdev-babuin-v3-server-main-tmcg33.streamlit.app', 8080))
+serv_sock.listen(10)
 
-app = Flask(__name__)
+while True:
+    client_sock, client_addr = serv_sock.accept()
+    print('Connected by', client_addr)
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        client_sock.sendall(data)
 
-@app.route('/login')
-def api():
-    secret_key = request.args.get('secret_key', default='my_default_value')
-    # Do something with the secret key
-    return str(API_system.account_exists())
-
-app.run()
+    client_sock.close()
